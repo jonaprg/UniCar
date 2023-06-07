6461f7465eaa6b20a04bba81dceff7ecb09e441e
Jonathan Rojas Granda <jonargp1@gmail.com>
2023-06-08 00:58:00 +0200

    Remove ratings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import morgan from 'morgan'
 import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import tripRoutes from './routes/tripsRoutes.js'
-import ratingRoutes from './routes/ratingRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,7 +13,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/trips', tripRoutes)
-app.use('/api/v1/ratings', ratingRoutes)
+// app.use('/api/v1/ratings', ratingRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
